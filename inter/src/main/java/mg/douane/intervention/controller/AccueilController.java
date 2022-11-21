a3731cfde6205024cd93db97600321846dc395da
Mle620 <f.mihajarivelo@etechconsulting-mg.com>
2022-11-21 23:13:04 +0300

    ito atao alo

---
+++ inter/src/main/java/mg/douane/intervention/controller/AccueilController.java
@@ -0,0 +1,14 @@
+package mg.douane.intervention.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AccueilController {
+
+    @RequestMapping(value = "/home")
+    public String accueilPage(Model model) {
+        return "home-view";
+    }
+}
